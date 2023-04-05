9ae6059e6b21cb10ce0ca7242b91ec8eef598fac
oszki <oszki.kontakt@gmail.com>
2023-04-05 15:20:20 +0200

    support for ranked grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,10 +9,11 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var totalStudents = Students.Count;
+            if (totalStudents < 5)
+                throw new InvalidOperationException();
+            
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+            var gradeIndex = grades.IndexOf(averageGrade);
+
+            if (gradeIndex < totalStudents * 0.2)
+                return 'A';
+            else if (gradeIndex < totalStudents * 0.4)
+                return 'B';
+            else if (gradeIndex < totalStudents * 0.6)
+                return 'C';
+            else if (gradeIndex < totalStudents * 0.8)
+                return 'D';
+            return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
